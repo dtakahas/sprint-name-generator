d1b00465bb543cc5c4cc50a776e88257fe17b5da
Dave Takahashi <dtcello@gmail.com>
2016-08-19 17:22:21 -0700

    Add custom lists

---
+++ src/sprint-name-generator/lists.cr
@@ -0,0 +1,200 @@
+ADJECTIVES = %w(
+  abandoned able absolute adorable adventurous academic acceptable acclaimed
+  accomplished accurate aching acidic acrobatic active actual adept admirable
+  admired adolescent adorable adored advanced afraid affectionate aged aggravating
+  aggressive agile agitated agonizing agreeable ajar alarmed alarming alert
+  alienated alive all altruistic amazing ambitious ample amused amusing
+  anchored ancient angelic angry anguished animated annual another antique
+  anxious any apprehensive appropriate apt arctic arid aromatic artistic
+  ashamed assured astonishing athletic  attached attentive attractive austere
+  authentic authorized automatic avaricious average aware awesome awful awkward
+  babyish bad back baggy bare barren basic beautiful belated beloved beneficial
+  better best bewitched big biodegradable bitter black bland blank blaring bleak
+  blind blissful blond blue blushing bogus boiling bold bony boring bossy both
+  bouncy bountiful bowed  brave breakable brief bright brilliant brisk broken
+  bronze brown bruised  bubbly bulky bumpy buoyant burdensome burly bustling
+  busy buttery buzzing calculating calm candid canine capital carefree careful
+  careless caring cautious cavernous celebrated charming  cheap cheerful cheery
+  chief chilly chubby circular classic clean clear clever close closed
+  cloudy clueless clumsy cluttered coarse cold colorful colorless colossal
+  comfortable common compassionate competent complete  complex complicated
+  composed concerned concrete confused conscious considerate constant content
+  conventional cooked cool cooperative  coordinated corny corrupt costly
+  courageous courteous crafty crazy creamy creative creepy criminal crisp
+  critical crooked crowded cruel crushing cuddly cultivated cultured cumbersome
+  curly curvy cute cylindrical damaged damp dangerous dapper daring darling
+  dark dazzling dead deadly deafening dear dearest  decent decimal decisive
+  deep defenseless defensive defiant deficient definite definitive delayed
+  delectable delicious  delightful delirious demanding dense dental dependable
+  dependent descriptive deserted detailed determined devoted different  difficult
+  digital diligent dim dimpled dimwitted direct disastrous discrete disfigured
+  disgusting disloyal dismal  distant downright dreary dirty disguised dishonest
+  dismal distant distinct distorted dizzy dopey  doting double downright drab
+  drafty dramatic dreary droopy dry dual dull dutiful each eager earnest early
+  easy ecstatic edible educated  elaborate elastic elated elderly
+  electric elegant elementary elliptical embarrassed  embellished eminent emotional
+  emotive empty enchanted enchanting energetic enlightened enormous  enraged entire
+  envious equal equatorial essential esteemed ethical euphoric  even evergreen
+  everlasting every evil exalted excellent exemplary exhausted excitable  excited
+  exciting exotic expensive experienced expert extraneous extroverted
+  fabulous failing faint fair faithful fake false familiar famous fancy fantastic
+  far faraway fast fat fatal fatherly favorable favorite fearful
+  fearless feisty feline female feminine few fickle  filthy fine finished firm first
+  firsthand fitting fixed flaky flamboyant flashy flat flawed flawless flickering
+  flimsy flippant flowery fluffy fluid flustered focused fond foolhardy foolish
+  forceful forked formal forsaken  forthright fortunate fragrant frail frank
+  frayed free French fresh frequent friendly frightened frightening frigid
+  frilly frizzy frivolous front frosty frozen frugal fruitful full fumbling
+  functional funny fussy fuzzy gargantuan gaseous general generous gentle
+  genuine giant giddy gigantic  gifted giving glamorous glaring glass gleaming
+  gleeful glistening glittering  gloomy glorious glossy glum golden good
+  gorgeous graceful  gracious grand grandiose granular grateful grave gray
+  great greedy green  gregarious grim grimy gripping grizzled gross grotesque
+  grouchy grounded  growing growling grown grubby gruesome grumpy guilty
+  gullible gummy hairy half handmade handsome handy happy hard harmful harmless
+  harmonious harsh hasty hateful haunting  healthy heartfelt hearty heavenly
+  heavy hefty helpful helpless  hidden hideous high hilarious hoarse hollow
+  homely  honest honorable honored hopeful horrible hospitable hot huge
+  humble humiliating humming humongous hungry hurtful husky icky icy ideal
+  idealistic identical idle idiotic idolized ignorant ill illegal
+  illiterate illustrious imaginary imaginative immaculate immaterial immediate
+  immense impassioned  impeccable impartial imperfect imperturbable impish
+  impolite important impossible impractical impressionable impressive improbable
+  impure inborn incomparable incompatible incomplete inconsequential incredible
+  indelible inexperienced indolent infamous infantile  infatuated inferior
+  infinite informal innocent insecure insidious insignificant insistent
+  instructive insubstantial  intelligent intent intentional interesting internal
+  international intrepid ironclad irresponsible irritating itchy jaded jagged
+  jaunty jealous jittery  joint jolly jovial  joyful joyous jubilant
+  judicious juicy jumbo  junior jumpy juvenile kaleidoscopic keen  key kind
+  kindhearted kindly  klutzy knobby  knotty knowledgeable knowing  known kooky
+  kosher lame lanky large last lasting late lavish lawful  lazy leading lean
+  leafy left legal legitimate light  lighthearted likable likely limited limp
+  limping linear lined liquid  little live lively livid loathsome lone lonely
+  long loose lopsided lost loud lovable lovely loving  low loyal
+  lucky lumbering luminous lumpy lustrous luxurious mad magnificent
+  majestic major male mammoth married marvelous  masculine massive mature
+  meager mealy mean measly meaty medical mediocre  medium meek mellow melodic
+  memorable menacing merry messy metallic mild  milky mindless miniature minor
+  minty miserable miserly misguided misty mixed  modern modest moist monstrous
+  monthly monumental moral mortified motherly motionless  mountainous muddy
+  muffled multicolored mundane murky mushy musty muted mysterious naive narrow
+  nasty natural naughty  nautical near neat necessary needy  negative neglected
+  negligible neighboring nervous new  next nice nifty nimble nippy  nocturnal
+  noisy nonstop normal notable noted  noteworthy novel noxious numb nutritious
+  nutty obedient obese oblong oily oblong obvious occasional  odd oddball
+  offbeat offensive official old only open optimal optimistic
+  opulent  orange orderly organic ornate ornery ordinary
+  original other our outlying outgoing outlandish  outrageous outstanding
+  oval overcooked overdue overjoyed overlooked palatable pale paltry parallel
+  parched partial passionate past pastel peaceful peppery perfect perfumed
+  periodic perky personal pertinent pesky pessimistic petty phony physical
+  piercing pink pitiful plain  plaintive plastic playful pleasant pleased
+  pleasing plump plush polished polite political pointed pointless poised
+  poor popular portly posh positive possible potable powerful powerless
+  practical precious present prestigious  pretty precious previous pricey
+  prickly primary prime pristine private prize probable productive profitable
+  profuse proper proud prudent punctual pungent puny pure purple pushy putrid
+  puzzled puzzling quaint qualified  quarrelsome quarterly  queasy querulous
+  questionable quick quiet quintessential  quirky quixotic
+  quizzical radiant ragged rapid rare rash raw recent reckless rectangular
+  ready real realistic reasonable red reflecting regal regular  reliable
+  relieved remarkable remorseful remote repentant required respectful
+  responsible  repulsive revolving rewarding rich rigid right ringed ripe
+  roasted robust rosy rotating rotten rough round rowdy  royal rubbery rundown
+  ruddy rude runny rural rusty sad safe salty same sandy sane sarcastic
+  sardonic satisfied scaly scarce scared scary scented scholarly scientific
+  scornful scratchy scrawny second secondary secret
+  selfish sentimental  separate serene serious serpentine several severe shabby
+  shadowy shady shallow shameful shameless sharp shimmering shiny shocked
+  shocking shoddy short showy shrill shy sick silent silky
+  silly silver similar simple simplistic sinful single sizzling skeletal
+  skinny sleepy slight slim slimy slippery slow slushy small smart smoggy
+  smooth smug snappy snarling sneaky sniveling snoopy  sociable soft soggy
+  solid somber some spherical sophisticated sore sorrowful soulful soupy sour
+  sparkling sparse specific spectacular speedy spicy spiffy spirited spiteful
+  splendid spotless spotted spry  square squeaky squiggly stable staid stained
+  stale standard starchy stark starry steep sticky stiff stimulating stingy
+  stormy straight strange steel strict strident striking striped strong studious
+  stunning  stupendous stupid sturdy stylish subdued submissive substantial
+  subtle suburban sudden sugary sunny super superb superficial superior
+  supportive surprised suspicious svelte sweaty sweet sweltering
+  swift sympathetic tall talkative tame tan tangible tart tasty tattered taut
+  tedious teeming  tempting tender tense tepid terrible terrific testy thankful
+  that these  thick thin third thirsty this thorough thorny those thoughtful
+  threadbare  thrifty thunderous tidy tight timely tinted tiny tired torn total
+  tough traumatic treasured tremendous tragic trained tremendous triangular
+  tricky trifling trim  trivial troubled true trusting trustworthy trusty truthful
+  tubby turbulent twin ugly ultimate unacceptable unaware uncomfortable uncommon
+  unconscious understated unequaled  uneven unfinished unfit unfolded unfortunate
+  unhappy unhealthy uniform unimportant unique  united unkempt unknown unlawful
+  unlined unlucky unnatural unpleasant unrealistic  unripe unruly unselfish
+  unsightly unsteady unsung untidy untimely untried  untrue unused unusual
+  unwelcome unwieldy unwilling unwitting unwritten upbeat  upright upset
+  urban usable used useful useless utilized utter vacant vague vain valid
+  valuable vapid variable vast velvety  venerated vengeful verifiable vibrant
+  vicious  victorious vigilant vigorous villainous violet  violent virtual
+  virtuous visible  vital vivacious vivid voluminous wan warlike warm warmhearted
+  warped wary wasteful watchful waterlogged watery wavy  wealthy weak weary
+  webbed wee weekly weepy weighty weird welcome wet which whimsical whirlwind
+  whispered  white whole whopping wicked wide wiggly wild willing
+  wilted winding windy  winged wiry wise witty wobbly woeful wonderful wooden
+  woozy wordy worldly worn  worried worrisome worse worst worthless worthwhile
+  worthy wrathful wretched writhing wrong wry
+)
+
+NOUNS = %w(
+  hangdown battery coder forest coffee latte mocha
+  needle thread garment beetle container toolbox
+  waterfall river breeze moon rain wind sea morning
+  snow lake sunset pine shadow leaf dawn glitter forest
+  hill cloud meadow sun glade bird brook butterfly
+  bush dew dust field fire flower firefly feather grass
+  haze mountain night pond darkness snowflake silence
+  sound sky shape surf thunder violet water wildflower
+  wave water resonance sun wood dream cherry tree fog
+  frost voice paper frog smoke star panda smell odor fragrance
+  kitten roll stranger adjustment truck sleep steam ocean grade sound education
+  ladybug knee dinner market tray stitch shade ray business discussion underwear
+  achiever lamp territory juice face existence jump worm knife base teeth arm
+  river power hour boy detail behavior flock rifle egg island liquid afterthought
+  wish temper vase marble humor cough stick prose move branch meat pocket zinc
+  coach coast locket coal sand blood quince sheet experience mailbox support
+  silk flame home friend camera sense summer scarecrow giants cause porter jelly
+  chin match pull sidewalk rat question pets plantation start touch mist play
+  order flower hospital design basketball chess thunder passenger sofa fact north
+  bag fairies library limit key boot morning ear afternoon aftermath riddle bell
+  card shirt baseball dog texture tooth cakes tiger size honey insurance
+  fireman produce letter driving brick eyes bird sneeze icicle request pot
+  trousers bubble motion burst box suit picture story pigs finger soap thing
+  bulb tank mom effect slope bike fear cows screw trip loaf chickens books fog
+  language berry bat hole needle scene attraction basin government year desk
+  quilt self dad noise cobweb week current gun stage stomach alarm thrill rub
+  tendency writing shoes whistle color birthday men blade care tin pump rainstorm
+  children kittens girl wool mother instrument bushes sink point shame dust
+  hands bottle appliance taste zephyr watch property yam rod trees cent sack
+  wave song action space soda queen duck wind cover minister wax straw turkey
+  zipper jar walk toy religion door sleet punishment side wood toothbrush collar
+  anger angle country ring wire machine fire quarter competition airplane slip
+  spiders price frame whip house umbrella jellyfish calendar health foot word hill
+  railway hydrant snake sponge scent quill day nest cake spy need mask
+  observation leg button tub coat teaching sign amount advice weight
+  payment cream twist wren reading voyage oil cook deer profit zebra glove
+  potato trail love office battle cracker stamp pin window sea yoke church
+  pickle peace cup cast pig snakes nerve toe quiver toes sheep title pet slave
+  bead engine boat lunch smoke fly stream rain grass rail hand selection offer
+  knot clam chance fowl earthquake oatmeal badge houses tomatoes drink
+  grandmother seashore vessel maid scissors governor cactus reward bedroom ship
+  apparatus breath distance direction amusement error air system guide uncle
+  soup canvas servant laugh work powder plane hate distribution history book
+  frog secretary flag jewel channel condition bells ice pie stocking carriage
+  pleasure butter cemetery ants lettuce furniture iron way mouth ink leather
+  border snow cave tax join son drop shelf elbow hair throat credit debt reason
+  bucket downtown swim part brake spot shock spoon horses horn head beds trouble
+  degree desire rice can chicken holiday vacation aunt unit swing front purpose
+  agreement cars street crack talk decision skin crowd smell pan wealth robin
+  cap industry dinosaurs receipt nation bed roof cattle theory thought farm tail
+  skate dime edge eggs baby top minute cats hobbies train fork authority heat
+  floor mitten crow destruction relation thread party sun wash apparel lip
+  number development addition kiss jeans range
+)
